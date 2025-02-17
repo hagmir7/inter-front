615a3305659845bb38a0c8d3b7cff94d88679ec3
Hassan AGHMIR <hagmir6@gmail.com>
2025-02-17 17:16:58 +0100

    Firt React conifgurations

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Product from './components/Product'
+
+function App() {
+
+  return (
+    <div className='mx-auto max-w-7xl mt-6'>
+      <Product />
+    </div>
+  )
+}
+
+export default App
